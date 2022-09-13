24a72794710acb4040afe3b1556072f853454ff6
daniel-britos <danielbritospersonal@gmail.com>
2022-09-13 13:40:04 -0300

    Se agrega ReadKey

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,7 +10,8 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");    
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
         }
     }
 }
