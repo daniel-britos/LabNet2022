31362cb450ca0e89997a749451c1b40726696379
daniel-britos <danielbritospersonal@gmail.com>
2022-10-02 22:46:24 -0300

    utest

---
+++ LinQ.Lab/LinQ.Lab.LogicTests/ClassLogic/ProductsLogicTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LinQ.Lab.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinQ.Lab.Data;
+
+namespace LinQ.Lab.Logic.Tests
+{
+    [TestClass()]
+    public class ProductsLogicTests
+    {
+        [TestMethod()]
+        public void GetProductsStockTest()
+        {            
+            ProductsLogic list = new ProductsLogic();
+            var stock = list.GetProductsStock();
+            var countInDb = 5; 
+
+            Assert.AreEqual(countInDb, stock.Count);
+        }
+    }
+}
